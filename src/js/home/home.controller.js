828cb82ffe627a9ca1cb22327b1b4554051e357e
Phillip <Phillip@192.168.1.9>
2016-08-23 21:35:04 -0400

    dynamically create clips

--- src/js/home/home.controller.js
+++ src/js/home/home.controller.js
@@ -1,9 +1,35 @@
 class HomeCtrl {
-  constructor(AppConstants) {
+  constructor(AppConstants, $sce, $scope) {
     'ngInject';
     
     this.appName = AppConstants.appName;
-
+    this.source = 'http://www.w3schools.com/tags/movie.mp4';
+    this.start = 4;
+    this.end = 10;
+    this.showVideo = true;
+    this.clips = [];
+    var video = this.source + '#t='+this.start+',' + this.end;
+    this.clips.push(video);
+    this.addClip = function(src, start, end){
+    	var video = this.source + '#t='+ start + ',' + end;
+        this.clips.push(video);
+    }
+    this.getSrc = function(src){
+    	return trustSrc(src);
+    	
+    }
+    function trustSrc(src) {
+        return $sce.trustAsResourceUrl(src);
+    }
+    
+    this.change = function(d){
+    	this.showVideo = false;
+    	console.log('d', d);
+    	this.showVideo = true;
+    }
+    $scope.$watch('start', function(c){
+    	console.log('c',c);
+    })
   }
 
 
