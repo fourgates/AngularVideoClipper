b04569681484cf67c9870b022cc5f9b1d4c619f4
Phillip <Phillip@192.168.1.9>
2016-08-23 22:04:38 -0400

    TODO

--- src/js/video/index.js
+++ src/js/video/index.js
@@ -4,3 +4,22 @@ var module = angular.module('app.video', []);
 
 import videoEditor from "./video-editor.directive";
 module.directive('videoEditor', videoEditor);
+
+// TODO:
+//		1. Cannot delete first video (first video is sample)
+//		2. Add Clip Interface
+//			a. Mechanism to create a clip given a Title, Start Time, and End time
+//			b. Table of Clips (Play Button, Select Checkbox, Title, Start, End, Delete Button)
+//			c. On row click play clip in table
+//		3. Ability Edit Existing Clips
+//		4. Input link for source video
+//		5. Break directive into components so the video player and playlist can be reused throughout the app
+//			a. maybe create an attribute directive?
+//		Bonus
+//		1. Ability to upload video
+//		2. Persist clips in cache
+//		3. Add tags to clips
+//		4. Filter by tags
+//		5. Hotkey to play the next clip
+//		6. Ability to queue clips
+//		7. Ability to sort clips (sort by index?)
