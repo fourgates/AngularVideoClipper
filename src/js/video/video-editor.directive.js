d7dc6fc197fec9d23c201b486ef45561024d9203
Phillip <Phillip@192.168.1.9>
2016-08-25 21:16:14 -0400

    added the ability to edit and delete clips. added auto-play functionality

--- src/js/video/video-editor.directive.js
+++ src/js/video/video-editor.directive.js
@@ -10,8 +10,6 @@ function VideoEditor($sce, $timeout){
 			'ngInject';
 			var ctrl = this;
 			ctrl.source = 'http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4';
-			ctrl.start = 4;
-			ctrl.end = 10;
 			ctrl.showVideo = true;
 			
 			if(!$scope.clips || $scope.clips.length == 0){
@@ -45,11 +43,8 @@ function VideoEditor($sce, $timeout){
 		    ctrl.playVideo = function(video){
 		    	$scope.start = video.start;
 		    	$scope.end = video.end;
-		    	$scope.video = null
-		    	$timeout(function(){
-		    		$scope.reload();
-			    	$scope.selectedVideo = video;
-		    	}, 2000)
+		    	$scope.reload();
+		    	$scope.selectedVideo = video;
 		    	
 		    }
 		    
@@ -58,26 +53,43 @@ function VideoEditor($sce, $timeout){
 		    	console.log('d', d);
 		    	ctrl.showVideo = true;
 		    }
+		    
+		    ctrl.deleteVideo = function(clip){
+		    	if(clip){
+		    		var index = $scope.clips.indexOf(clip);
+		    		if(index > -1){
+		    			$scope.clips.splice(index, 1);
+		    		}
+		    	}
+		    }
 		},
 		link: function(scope, element, attrs, ctrl, transclude){
 			scope.$watch('clips', updatePlayer);
 			
 			function updatePlayer(){
+				console.log('scope', ctrl);
 				scope.selectedVideo = null;
 				var el = $(element);
 				scope.player = el.find("video")[0];
+				
 				//scope.player.empty();
 				if(scope.player){
 					scope.player.height = scope.player.height;
 					//scope.player.paused = false;
 					scope.player.currentTime = scope.start;
-					console.log('start', scope.start);
-					console.log('el', el[0]);
-					console.log('video', scope.player);
-					console.log('video1', el.find("video"));
+					scope.player.play();
+					scope.player.addEventListener("timeupdate",function(event) {console.log('!*!*!*!!*tes', event.timeStamp / 1000)});
 				}
 				
 			}
+			function getTime(){
+				var el = $(element);
+				var video = el.find("video")[0];
+				if(video){
+					return video.currentTime;
+				}
+			}
+			scope.test = getTime;
 			scope.reload = updatePlayer;
 		}
 	}
