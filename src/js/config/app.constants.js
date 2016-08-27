2f818e809239d623154d95733035b13685ebb1e2
Phillip <Phillip@192.168.1.9>
2016-08-27 18:31:34 -0400

    changed app name

--- src/js/config/app.constants.js
+++ src/js/config/app.constants.js
@@ -2,7 +2,7 @@ const AppConstants = {
   //api: 'http://localhost:3000/api',
 	api: 'https://conduit.productionready.io/api',
 	jwtKey: 'jwtToken',
-	appName: 'fourgates',
+	appName: 'angualr video clipper',
 	secure: false
 };
 // TODO - upload video from cell phone = http://stackoverflow.com/questions/5238175/how-can-i-use-java-to-download-a-video-file-from-http-urlvideo-streaming-link
